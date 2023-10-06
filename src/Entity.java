78cecd5ee28265f2114759c7f2ec26c175a3f715
trofimovJava23 <trofimovJava23@gmail.com>
2023-10-06 13:02:29 +0300

    Creating New Classes
    Entity, Monster, Player

---
+++ src/Entity.java
@@ -0,0 +1,22 @@
+import java.lang.Double;
+
+public class Entity {
+
+int attack; //entered [1..30] , need init
+int defence; //entered [1..30] , need init
+
+
+int health; //entered [1..healthMax], need init
+int healthMax;
+int numberOfHeals = 0; //entered [0..4]
+public void Heal() {
+	
+}
+
+int damage[]; //collection of int M..N
+public void Fight() {
+	
+}
+
+
+}
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+
+public class Player extends Entity {
+
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
